6e3e330f1623be74ec3da7e61605eab6aefb8f87
AlexiaTdev <alexia.teissier1@epsi.fr>
2022-02-27 15:18:53 +0100

    step 10 flex props & state

--- GeekFit/App.js
+++ GeekFit/App.js
@@ -3,22 +3,23 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
 import ClassHome from './components/ClassHome';
+import Contants from 'expo-constants';
 
 export default function App() {
   
   return (
     <View style={styles.container}>
       <Home />
-      <ClassHome name = "alexia"/>
+      
     </View>
   );
 }
+//<ClassHome name = "alexia"/>
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eddfdf',
+    marginTop:Contants.statusBarHeight
   },
 });
--- GeekFit/components/Home.js
+++ GeekFit/components/Home.js
@@ -1,18 +1,60 @@
 import React, {useState} from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import {Card, FAB} from 'react-native-paper'
+
 
 function Home() {
-    const [name, setName] = useState('AlexiaTB')
+   const data = [
+       {id:1, title:'First Title', body:'First Body'},
+       {id:2, title:'Second Title', body:'Second Body'},
+       {id:3, title:'Third Title', body:'Third Body'}
+   ]
+
+   const renderData = (item) => {
+       return (
+           <Card style = {styles.cardStyle}>
+               <Text style = {{fontSize:20}}>{item.title}</Text>
+               <Text>{item.body}</Text>
+           </Card>
+
+       )
+   }
   return (
-      <>
-            <Text>Open up App.js to start working on your app!</Text>
-            <Text>{name}</Text>
-            <Button title="clicme" onPress={()=>setName('hell')}></Button>
-      </>
+      <View style = {{flex:1}}>
+          <FlatList
+          data = {data}
+          renderItem = {({item}) => {
+              return renderData(item)
+          }}
+          keyExtractor = {item => `${item.id}`}
+          />
+          <FAB
+          style = {styles.fab}
+          small={false}
+          icon="plus"
+          theme={{colors:{accent:"green"}}}
+          onPress = {()=> console.log("yes")}
+          />
+      </View>
 
 
 
   )
 }
+
+const styles = StyleSheet.create({
+    cardStyle: {
+        padding:10,
+        margin:10
+    },
+
+    fab: {
+        position:'absolute',
+        margin:16,
+        right:0,
+        bottom:0
+    }
+
+})
 
 export default Home
